f83b5c5085a7c2240c37aafda2b7f80cb163de50
Artem Shalamov <iart1756@gmail.com>
2020-03-22 19:20:22 +0500

    refactoring profile and dialogs reducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -13,34 +13,34 @@ let initialState = {
 
 const profileReducer = (state = initialState, action) => {
 
-    switch(action.type) {
+    switch (action.type) {
         case ADD_POST: {
             let newPost = {
                 id: 5,
                 message: state.newPostText,
                 likesCount: 0
             };
-            let stateCopy = {...state}
+            return {
+                ...state,
+                posts: [...state.posts, newPost],
+                newPostText: ''
+            };
 
-            stateCopy.posts = [...state.posts]
-            stateCopy.posts.push(newPost)
-            stateCopy.newPostText = '';
-            return stateCopy;
         }
         case UPDATE_NEW_POST_TEXT: {
-            let stateCopy = {...state}
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
+            return {
+                ...state,
+                newPostText: action.newText
+            };
         }
         default:
             return state;
 
     }
-}
-
+};
 
 export const addPostActionCreator = () => ({type: ADD_POST})
 export const updateNewPostTextActionCreator = (text) =>
-    ({type: UPDATE_NEW_POST_TEXT, newText: text })
+    ({type: UPDATE_NEW_POST_TEXT, newText: text})
 
 export default profileReducer;
