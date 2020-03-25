9da3c18787264d2dbd2b695633f9a709e6393984
Artem Shalamov <iart1756@gmail.com>
2020-03-25 19:38:20 +0500

    add pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,19 +5,40 @@ import userPhoto from '../../assets/images/user.jpg'
 
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            this.props.setUsers(response.data.items)
-        })
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount)
+            })
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            })
+    }
 
-render()
-{
-    return <div>
-        {
-            this.props.users.map(u => <div key={u.id}>
+    render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
+
+        return <div>
+            <div>
+                { pages.map( p => {
+                    return <span className={this.props.currentPage === p && styles.selectedPage}
+                    onClick={(e) => { this.onPageChanged((p)) }}>{p}</span>
+                })}
+            </div>
+            {
+                this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
                         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
@@ -33,7 +54,7 @@ render()
                             }}>Follow</button>}
                     </div>
                 </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -43,10 +64,10 @@ render()
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
-}
+                </div>)
+            }
+        </div>
+    }
 }
 
 
